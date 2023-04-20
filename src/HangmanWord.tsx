c21e22b8e82d515df8bde20e9c790b1ab0cf1ce8
Akindejoye <tydruhill@gmail.com>
2023-04-20 04:48:41 +0100

    HangmanWord Component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,6 +1,30 @@
 const HangmanWord = () => {
+  const word = "test";
+  const guessedLetter = ["t", "e", "f"]
+
   return ( 
-    <h1>HangmanWord</h1>
+    <div
+     style={{
+      display: "flex",
+      gap: ".25em",
+      fontSize: "6rem",
+      fontWeight: "bold",
+      textTransform: "uppercase",
+      fontFamily: "monospace",
+     }}>
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black"}} key={index}>
+          <span style={{ 
+            visibility: guessedLetter.includes(letter)
+              ? "visible"
+              : "hidden"
+            }}
+          >
+          {letter}
+          </span>
+        </span>
+      ))}
+     </div>
    );
 }
  
